bd0e7420b7d60492390854f23b996dfc1c8ad190
sirashutoshbahuguna <66209378+sirashutoshbahuguna@users.noreply.github.com>
2021-03-15 01:48:37 +0530

    editted fiirst commit

--- polling.c
+++ polling.c
@@ -1,16 +1,22 @@
 #include<stdio.h>
+#include<windows.h>
 
 #define CANDIDATE_COUNT
 
-#define CANDIDATE1 "David Hull"
-#define CANDIDATE2 "Kristin Canella"
-#define CANDIDATE3 "Jim Brar"
+#define CANDIDATE1 "Narendra Modi"
+#define CANDIDATE2 "Rahul Gandhi"
+#define CANDIDATE3 "Mamta Banerjee"
 #define CANDIDATE4 "Donald Truimph"
 
-int votesCount1=0, votesCount2=0, votesCount3=0, votesCount4=0, spoiledtvotes=0;
+int votesCount1=0;
+int votesCount2=0;
+int votesCount3=0;
+int votesCount4=0;
+int spoiledtvotes=0;
 
-void castVote(){
-int choice;    
+void castVote()
+{
+int choice;
 printf("\n\n ### Please choose your Candidate ####\n\n");
 printf("\n 1. %s", CANDIDATE1);
 printf("\n 2. %s", CANDIDATE2);
@@ -21,12 +27,18 @@ printf("\n 5. %s", "None of These");
 printf("\n\n Input your choice (1 - 4) : ");
 scanf("%d",&choice);
 
-switch(choice){
-    case 1: votesCount1++; break;
-    case 2: votesCount2++; break;
-    case 3: votesCount3++; break;
-    case 4: votesCount4++; break;
-    case 5: spoiledtvotes++; break;
+switch(choice)
+{
+    case 1: votesCount1++;
+    break;
+    case 2: votesCount2++;
+    break;
+    case 3: votesCount3++;
+    break;
+    case 4: votesCount4++;
+    break;
+    case 5: spoiledtvotes++;
+    break;
     default: printf("\n Error: Wrong Choice !! Please retry");
              //hold the screen
              getchar();
@@ -34,16 +46,18 @@ switch(choice){
 printf("\n thanks for vote !!");
 }
 
-void votesCount(){
-printf("\n\n ##### Voting Statics ####");
+void votesCount()
+{
+printf("\n\n ##### Voting Statistics ####");
 printf("\n %s - %d ", CANDIDATE1, votesCount1);
-printf("\n %s - %d ", CANDIDATE1, votesCount2);
-printf("\n %s - %d ", CANDIDATE1, votesCount3);
-printf("\n %s - %d ", CANDIDATE1, votesCount4);
-printf("\n %s - %d ", "Spoiled Votes", spoiledtvotes); 
+printf("\n %s - %d ", CANDIDATE2, votesCount2);
+printf("\n %s - %d ", CANDIDATE3, votesCount3);
+printf("\n %s - %d ", CANDIDATE4, votesCount4);
+printf("\n %s - %d ", "Spoiled Votes", spoiledtvotes);
 }
 
-void getLeadingCandidate(){
+void getLeadingCandidate()
+{
     printf("\n\n  #### Leading Candiate ####\n\n");
     if(votesCount1>votesCount2 && votesCount1>votesCount3 && votesCount1 >votesCount4)
     printf("[%s]",CANDIDATE1);
@@ -54,10 +68,10 @@ void getLeadingCandidate(){
     else if(votesCount4>votesCount1 && votesCount4>votesCount2 && votesCount4 >votesCount3)
     printf("[%s]",CANDIDATE4);
     else
-    printf("----- Warning !!! No-win situation----");    
-    
-    
-    
+    printf("----- Warning !!! No-win situation----");
+
+
+
 }
 
 int main()
@@ -65,8 +79,23 @@ int main()
 int i;
 int choice;
 
-do{
-printf("\n\n ###### Welcome to Election/Voting 2019 #####");
+system("color A");
+	printf(" -------------------------------------------------------------------------\n");
+	printf("|                                                                         |\n");
+	printf("|                                                                         |\n");
+	printf("|  OOOOOO   OOOOOO OOOOOO OOOOOO OOOOOO OOOOOO O      O OOOOOOO  OOOOOO   |\n");
+	printf("|  O        O    O O      O        O      O    O O    O O        O        |\n");
+	printf("|  O  OOOOO OOOOOO OOOOO  OOOOO    O      O    O  O   O O  OOOOO OOOOOO   |\n");
+	printf("|  O    O   O  O   O      O        O      O    O   O  O O    O        O   |\n");
+	printf("|  OOOOOO   O   O  OOOOOO OOOOOO   O    OOOOOO O    O O OOOOOO   OOOOOO   |\n");
+	printf("|                                                                         |\n");
+	printf(" -------------------------------------------------------------------------\n");
+
+	printf(" This Project is Made By Ashutosh Bahuguna And Shubhansh Yadav \n");
+
+do
+{
+printf("\n\n ********Welcome to Election/Voting 2021**********");
 printf("\n\n 1. Cast the Vote");
 printf("\n 2. Find Vote Count");
 printf("\n 3. Find leading Candidate");
@@ -77,12 +106,17 @@ scanf("%d", &choice);
 
 switch(choice)
 {
-case 1: castVote();break;
-case 2: votesCount();break;
-case 3: getLeadingCandidate();break;
+case 1: castVote();
+break;
+case 2: votesCount();
+break;
+case 3: getLeadingCandidate();
+break;
 default: printf("\n Error: Invalid Choice");
+
 }
-}while(choice!=0);
+}
+while(choice!=0);
 
 //hold the screen
 getchar();
